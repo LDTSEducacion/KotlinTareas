3dad29cc05dcd2bbd8ded150e2c64d7d3ec699fa
Pablosfc95 <70.pablo@gmail.com>
2021-03-09 17:14:29 +0100

    Creando segunda activity para leer las tareas de firebase

---
+++ app/src/main/java/com/example/miprimeraapp/ReadFirebaseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.miprimeraapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ReadFirebaseActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_readfirebase)
+    }
+}
